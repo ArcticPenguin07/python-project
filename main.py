993c84c303291410c1c3dabd6633a5aa75aa7b11
Rafael Greif <rafaelgreif@freenet.de>
2022-02-22 10:28:08 +0100

    Add files via upload

---
+++ main.py
@@ -0,0 +1,26 @@
+print('Hallo Ruben!')
+
+name = 'Ruben'
+age = '12'
+
+print('Herzlichen Glückwunsch zum ' + age + ' Geburtstag!')
+
+is_12_years = True
+
+if is_12_years:
+    print('Du hast jetzt bestimmt schon einen Personalausweis erhalten.')
+
+else:
+    print('Dann musst du noch warten bis du 12 Jahre alt bist.')
+
+print('Du feierst bestimmt deinen Geburtstag am Wochenende im kleinen Familienkreis, oder?')
+
+question = input('Bitte Antwort angeben!')
+
+if input() != 'yes':
+    print('Dann feier schön deinen Geburtstag mit deiner Familie! :)')
+
+else:
+    print('')
+
+print('Auf Wiedersehen!')
